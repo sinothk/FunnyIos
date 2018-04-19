b8a375ecfc6b2f7787da5b21d61f22d3d7091c3d
LiangYuTao <sinothk@126.com>
2018-04-19 17:05:13 +0800

    添加ToastView

--- funny/funny/Classes/view/comm/LoginViewController.swift
+++ funny/funny/Classes/view/comm/LoginViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
+
 class LoginViewController: UIViewController {
-    @IBOutlet weak var registerBtn: UIBarButtonItem!
+    
     @IBOutlet weak var avatarIv: UIImageView!
     @IBOutlet weak var userCodeTxt: UITextField!
     @IBOutlet weak var userPwdTxt: UITextField!
     
+    @IBOutlet weak var registerBtn: UIBarButtonItem!
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var findPwdBtn: UIButton!
     
@@ -22,7 +24,7 @@ class LoginViewController: UIViewController {
 
         setupUI()
     }
-    // MARK: ->初始化界面
+    // MARK: -> 初始化界面
     func setupUI() -> Void {
         avatarIv.image = UIImage(named:"NewVersion3")
         avatarIv.layer.cornerRadius = 40
@@ -33,6 +35,10 @@ class LoginViewController: UIViewController {
         userPwdTxt.text = "123456"
         userPwdTxt.isSecureTextEntry = true
         
+        loginBtn.setTitleColor(UIColor.white, for: UIControlState.normal)
+        loginBtn.backgroundColor = themeColor
+        loginBtn.layer.cornerRadius = 6
+        
         registerBtn.action = #selector(titleBarRightBtn)
         loginBtn.addTarget(self, action: #selector(login), for: UIControlEvents.touchUpInside)
     }
@@ -41,10 +47,24 @@ class LoginViewController: UIViewController {
         print("注册")
     }
     
+    // MARK: -> 点击登录
     @objc func login(){
+        let userCode = userCodeTxt.text
+        let userPwd = userPwdTxt.text
         
+        if userCode==nil || userCode?.count == 0{
+            self.view.makeToast("请输入账号或邮箱", position: .center)
+            return
+        }
         
-        UserViewModel.shared.login(accountStr: "", pwdStr: "") { (isOk) in
+        if userPwd==nil || userPwd?.count == 0{
+            self.view.makeToast("请输入密码", position: .center)
+            return
+        }
+        
+        // 判断网络
+            
+        UserViewModel.shared.login(accountStr: userCode!, pwdStr: userPwd!) { (isOk) in
             if isOk {
                 print("isOk")
             } else{
